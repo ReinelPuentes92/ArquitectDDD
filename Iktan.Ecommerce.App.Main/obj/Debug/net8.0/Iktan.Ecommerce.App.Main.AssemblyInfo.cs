98b457c90bcc9b80b2dc25cf47b24e69f7dfeba8
Reinel Fernando Puentes Moreno <rpuentesm@ssf.gov.co>
2024-06-26 16:18:17 -0500

    Install DocFx Documentatio Project

--- Iktan.Ecommerce.App.Main/obj/Debug/net8.0/Iktan.Ecommerce.App.Main.AssemblyInfo.cs
+++ Iktan.Ecommerce.App.Main/obj/Debug/net8.0/Iktan.Ecommerce.App.Main.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Iktan.Ecommerce.App.Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29806b713e8507cdaedaac07d2b07a584bdc2c0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ece57642a931c890e9f51bbab53b37e88a5ff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Iktan.Ecommerce.App.Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Iktan.Ecommerce.App.Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
